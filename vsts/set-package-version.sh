176f75f881ca938996ac09e0c13e59f320be13e7
bailey everts <texas312@gmail.com>
2022-09-11 10:23:38 -0600

    chore: remove old vsts stuff

--- vsts/set-package-version.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $(dirname 0)
-VERSION=$(cat ../../package.json | grep -o 'version": ".*"' | sed 's/"//g' | cut -d " " -f 2).$BUILD_BUILDNUMBER
-echo "##vso[task.setvariable variable=version;]${VERSION}"
-echo "Version Set to $VERSION"
